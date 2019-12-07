6369c3842dd6c67e6ee4a7fbe253c9f7cedd6cf7
sarzz <45039724+sarzz2@users.noreply.github.com>
2019-12-07 23:47:00 +0530

    Update isograms.py

--- isograms.py
+++ isograms.py
@@ -8,7 +8,7 @@ def is_isogram(a):
             les.append(letter)
     return True                 #use print("True") if not getting any output in IDE
 a = input("Enter a word:")
-is_isogran(a)
+is_isogram(a)
 #An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
 #Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
 
