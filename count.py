50370e633c7d73227e70a81bc6cad6be4eeeb42e
sarzz <45039724+sarzz2@users.noreply.github.com>
2019-12-08 21:23:07 +0530

    Create count.py

---
+++ count.py
@@ -0,0 +1,12 @@
+def monkey_count(n):
+    ans = []
+    for i in range (0,n):
+        i += 1
+        ans.append(i)
+    return ans
+n = int(input("Enter the nummber")
+monkey_count(n)
+
+
+#Example Output if input = 4
+#['1','2','3','4']
