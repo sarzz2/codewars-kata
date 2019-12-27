f612db6b92e2df4c38fcd341c116a2cd6e021401
sarzz <45039724+sarzz2@users.noreply.github.com>
2019-12-28 01:18:18 +0530

    Update pin.py

--- pin.py
+++ pin.py
@@ -1,3 +1,6 @@
+#ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+
 def validate_pin(pin):
     if len(pin) == 4 or len(pin) == 6:
         if pin.isnumeric() is True:
@@ -8,4 +11,3 @@ def validate_pin(pin):
         return False  #use print("True") if not getting any output in IDE
 validate_pin(pin)
 
-#ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
